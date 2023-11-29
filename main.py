7d0fd38af95c54751142a39392dadc68e8360985
RajbharSachin <109137527+RajbharSachin@users.noreply.github.com>
2023-11-29 21:48:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from datetime import date
 
 import yfinance as yf
 from plotly import graph_objs as go 
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
+from prophet import Prophet
+from prophet.plot import plot_plotly
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
